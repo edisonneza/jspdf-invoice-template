9d0a50fa71b358dbec22ddb15674694e1eaffc23
eNezaLaptop <edisonneza@hotmail.com>
2021-02-10 16:57:07 +0100

    Changed libraryTarget to "var" to make it work in browser via CDN

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
   output: {
     path: path.resolve('dist'),
     filename: 'index.js',
-    libraryTarget: 'commonjs2',
+    library: "jsPDFInvoiceTemplate",
+    libraryTarget: 'var',
   },
   module: {
     rules: [

d53e36a21f7805178c96b2e76034006bc0d1db9c
eNezaDell <edisonneza@gmail.com>
2022-01-25 16:23:55 +0100

    Angular v13 test

---
+++ tests/angular/angular-jspdf-invoice-template/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+debugger;
+import jsPDFInvoiceTemplate from 'jspdf-invoice-template';
+// import jsPDFInvoiceTemplate from 'jspdf-invoice-template';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-jspdf-invoice-template';
+}
+
+
+// console.log(jsPDFInvoiceTemplate);
+console.log("jspdf", jsPDFInvoiceTemplate)
+
+
